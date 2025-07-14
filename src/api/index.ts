7c7f5c9fdb07ba70997bf36ff7e15fe39de09102
slantie <kandarp.gajjar.460@gmail.com>
2025-07-14 20:24:25 +0530

    Deployment Error Fixes - 3

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,9 @@
-// api/index.ts
 import app from '../app';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { setupScheduledTasks } from '../utils/scheduler';
 
-setupScheduledTasks(); // Optional: will only run once on cold start
+setupScheduledTasks(); // optional
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  app(req as any, res as any); // Let Express handle the rest
+  app(req as any, res as any);
 }
